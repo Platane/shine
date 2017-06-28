23fc38877d4975041967d5f44c204506d0dad95f
platane <me@platane.me>
2017-06-28 17:00:47 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const production = 'production' == process.env.NODE_ENV;
+const production = 'production' === process.env.NODE_ENV;
 
 module.exports = {
   entry: {
     index: ['./lib/js/src/app.js', './src/index.html'],
-    elk: ['./src/asset/elk.svg'],
-    ball: ['./src/asset/ball.svg'],
   },
 
   output: {
@@ -33,18 +31,15 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            },
-          },
-        ],
-      },
     ],
+  },
+
+  devServer: {
+    port: 8082,
+    contentBase: [path.join(__dirname, 'src/asset')],
+    watchOptions: {
+      ignored: /node_modules/,
+    },
   },
 
   plugins: [
@@ -52,7 +47,15 @@ module.exports = {
       // env var
       ['process.env.NODE_ENV']: `'${process.env.NODE_ENV}'`,
     }),
-  ],
+
+    new webpack.optimize.ModuleConcatenationPlugin(),
+
+    production &&
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: false,
+        comments: false,
+      }),
+  ].filter(Boolean),
 
   devtool: 'source-map',
 };
